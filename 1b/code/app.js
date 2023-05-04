43933089e697a36e3c17474ac3fa077c984602da
siddharth ghumare <97939703+siddharth2443@users.noreply.github.com>
2023-05-04 13:54:44 +0530

    4-05-23

---
+++ 1b/code/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#btnAddStudent").click(function () {
+        function getStudentData() {
+            let student = {
+                firstName: $("#firstName").val(),
+                lastName: $("#lastName").val(),
+            };
+            return student;
+        }
+        function storeDataToLocalStorage() {
+            if (!localStorage.getItem("student")) {
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            } else {
+                localStorage.removeItem("student");
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            }
+        }
+        storeDataToLocalStorage();
+        window.location.href="display.html"
+    });
+});
